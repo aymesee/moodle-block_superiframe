c2a859b44363cae63e05c098757e259197899e10
aymesee <amy.jenniches@gmail.com>
2016-04-28 15:49:11 -0400

    First commit

---
+++ lang/en/block_superiframe.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_superiframe', language 'en'
+ *
+ * @package   block_superiframe
+ * @copyright Daniel Neis <danielneis@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['superiframe:addinstance'] = 'Add a superiframe block';
+$string['superiframe:myaddinstance'] = 'Add a superiframe block to my moodle';
+$string['pluginname'] = 'superiframe';
+$string['welcomeuser'] = 'Welcome to superiframe, {$a->firstname} {$a->lastname}';
+$string['gotosuperiframe'] = 'Do you want to play a game?';
+$string['url_details'] = 'Put the URL of the iFrame you want to insert here.';
+$string['height_details'] = 'Height of the block';
+$string['width_details'] =  'Width of the block';
+$string['url'] = 'iFrame URL';
+$string['height'] = 'Height of the block';
+$string['width'] =  'Width of the block';
+$string['layout'] = 'What layout do you want to use?';
+$string['layout_details'] = 'You can use popup or course.';
+
